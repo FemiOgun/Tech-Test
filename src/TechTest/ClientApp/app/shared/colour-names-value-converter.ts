5a4020d3adbbc448e6523abbdd6c41458a6674c2
Femi <35222194+FemiOgun@users.noreply.github.com>
2019-04-15 19:00:46 +0100

    Add files via upload

---
+++ src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
@@ -0,0 +1,14 @@
+import { IColour } from '../people/interfaces/icolour';
+
+export class ColourNamesValueConverter {
+
+  toView(colours: IColour[]) {
+
+
+      var Colour = string.Join(",", Colour.ToArray());
+   
+
+    return 'Todo';
+  }
+
+}
---
+++ src/TechTest/Controllers/PeopleController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TechTest.Repositories;
+using TechTest.Repositories.Models;
+
+namespace TechTest.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public PeopleController(IPersonRepository personRepository)
+        {
+            this.PersonRepository = personRepository;
+        }
+
+        private IPersonRepository PersonRepository { get; }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var people = this.PersonRepository.GetAll();
+
+            return this.Ok(people);
+
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("GetAllPeoples")]
+        public JsonResult GetAllPeoples()
+        {
+            return Json(PersonRepository.getInstance().GetAll());
+        }
+
+           
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            if (!PersonRepository.Get(id, out var people))
+            {
+                return NotFound();
+            }
+
+            return people;
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Update(int id, PersonUpdate personUpdate)
+        {
+
+            if (!PersonRepository.Update(id, out var personUpdate))
+            {
+                return NotFound();
+            }
+
+
+        throw new NotImplementedException();
+        }
+    }
+}
